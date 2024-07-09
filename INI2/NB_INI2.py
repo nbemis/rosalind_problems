f6f3b2932c60c864d9353c7fd3dd30eb228f5e83
nbemis <nbemisderfer@gmail.com>
2024-07-08 21:07:55 -0500

    commit first two INI solutions for nb

---
+++ INI2/NB_INI2.py
@@ -0,0 +1,9 @@
+'''This function returns The integer corresponding
+to the square of the hypotenuse of the right triangle
+whose legs have lengths a and b.
+'''
+
+def calculate_hypotenuse(side_a_length, side_b_length):
+    return side_a_length ** 2 + side_b_length ** 2
+
+print calculate_hypotenuse(979,960)
